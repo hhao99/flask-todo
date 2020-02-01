a22b604ddae3ba364b2667eb8f9251b3fa0b52e4
hhao99 <hhao99@163.com>
2020-02-01 20:27:02 +0800

    update app logic

--- app.py
+++ app.py
@@ -3,6 +3,19 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return "HOME"
+
+@app.route('/hello')
 def hello():
     return "Hello World!"
+
+
+@app.route('/hello/<name>')
+def greeting(name):
+    return f"Hello {name}!"
+
+@app.route('/post/<int:id>')
+def post(id):
+    return f"get the id: {id}"
 
