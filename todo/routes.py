ebeda304739c1dae1efb6959a713955c6c8f9e6a
hhao99 <hhao99@163.com>
2020-02-02 12:22:18 +0800

    v2-todo add new task form and logic

--- todo/routes.py
+++ todo/routes.py
@@ -1,7 +1,19 @@
-from flask import render_template, render_template, request, g, flash
+from flask import (
+    render_template, redirect, render_template, 
+    request, g, flash, url_for
+)
 
+todos = []
 def init_route(app):
 
     @app.route('/')
     def index():
-        return render_template('index.html')
+        
+        return render_template('index.html',todos = todos)
+    @app.route('/new',methods=['GET','POST'])
+    def new():
+        if request.method == 'POST':
+            task = request.form['task']
+            todos.append({'task': task, 'isDone': False})
+            return redirect(url_for('index'))
+        return render_template('edit.html')
